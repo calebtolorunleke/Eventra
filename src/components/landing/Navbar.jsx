e76b28d3a11eb25d4ab3a46c25d16ad05571b3f6
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-06-28 21:05:57 -0700

    feat: create responsive navigation with mobile menu

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../common/Logo";
-import { Search } from "@mui/icons-material";
+import { Search, Menu as MenuIcon, Close } from "@mui/icons-material";
 
 const Navbar = () => {
+  const [Menu, setMenu] = useState(false);
   return (
-    <section className="w-full backdrop-blur-md bg-black/40 bg-opacity-50 mx-auto top-0">
-      <nav className="flex flex-row items-center justify-between px-8 py-5 max-w-[1250px] mx-auto">
+    <section className="w-full backdrop-blur-md bg-black/20 bg-opacity-50 mx-auto top-0">
+      {/* Desktop View */}
+      <nav className="lg:flex hidden flex-row items-center justify-between px-8 py-5 max-w-[1250px] mx-auto">
         <Logo />
-        <ul className="flex items-center gap-8 text-white font-bold">
+        <ul className="flex items-center gap-8 text-white text-md">
           <li>Discover Events</li>
           <li>About Us</li>
           <li>Contact</li>
@@ -18,7 +20,42 @@ const Navbar = () => {
             sx={{ fontSize: 28 }}
           />
           <button>Sign in</button>
-          <button>Get Started</button>
+          <button className="bg-green-900 px-4 py-2 rounded-md text-sm">
+            Get Started
+          </button>
+        </div>
+      </nav>
+
+      {/* Phone View  */}
+      <nav className="relative flex lg:hidden flex-row items-center justify-between px-8 py-5 max-w-[1250px] mx-auto">
+        {" "}
+        <Logo />
+        {Menu && (
+          <ul className="absolute left-0 top-full w-full bg-black flex flex-col items-center gap-8 p-8 text-white">
+            <li>Discover Events</li>
+            <li>About Us</li>
+            <li>Contact</li> <button>Sign in</button>
+            <button className="bg-green-900 px-4 py-2 rounded-md text-sm">
+              Get Started
+            </button>
+          </ul>
+        )}
+        <div className="flex flex-row items-center gap-6 text-white font-bold">
+          <Search
+            className="cursor-pointer  text-white "
+            sx={{ fontSize: 28 }}
+          />
+          <button
+            onClick={() => setMenu(!Menu)}
+            className="cursor-pointer"
+            aria-label={Menu ? "Close menu" : "Open menu"}
+          >
+            {!Menu ? (
+              <MenuIcon sx={{ fontSize: 32 }} />
+            ) : (
+              <Close sx={{ fontSize: 32 }} />
+            )}
+          </button>
         </div>
       </nav>
     </section>
