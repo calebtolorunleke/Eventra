2b0fa2386c5badda0c596b5ede20fd329df9477e
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-05 23:44:00 -0700

    feat: finalize landing page before authentication implementation

--- src/components/landing/Testimonials.jsx
+++ src/components/landing/Testimonials.jsx
@@ -9,8 +9,9 @@ export const Testimonials = () => {
     dots: true,
     infinite: true,
     speed: 600,
-    slidesToShow: 3,
     slidesToScroll: 1,
+    slidesToShow: 1,
+    mobileFirst: true,
     autoplay: true,
     autoplaySpeed: 3500,
     pauseOnHover: true,
@@ -19,12 +20,16 @@ export const Testimonials = () => {
     cssEase: "ease-in-out",
     responsive: [
       {
-        breakpoint: 1024,
-        settings: { slidesToShow: 2 },
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+        },
       },
       {
-        breakpoint: 768,
-        settings: { slidesToShow: 1 },
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
       },
     ],
   };
