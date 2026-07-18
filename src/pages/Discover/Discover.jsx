5f9b1f6a417cc9f2cc89aa7e64dc43e4e7938d09
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-18 00:32:33 -0700

    refactor: make Hero component reusable with props

--- src/pages/Discover/Discover.jsx
+++ src/pages/Discover/Discover.jsx
@@ -1,10 +1,25 @@
 import Hero from "@/components/landing/Hero";
 import React from "react";
+import background from "../../assets/images/landingPageBg.jpg";
 
 const Discover = () => {
+  const Herodata = [
+    {
+      id: 1,
+      title: "Discover Events That Inspire and Connect",
+      description:
+        "Your complete destination for discovering, booking, and experiencing events that truly matter to you.",
+      image: background,
+      button: "Explore Events",
+    },
+  ];
   return (
     <section>
-      <Hero />
+      <div>
+        {Herodata.map((data) => (
+          <Hero key={data.id} data={data} />
+        ))}
+      </div>
     </section>
   );
 };
