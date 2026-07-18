bdd2045de28aae993b4487f19b044daf4371135b
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-18 00:05:10 -0700

    feat: implement the layout for nvabar and footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/Landing/LandingPage";
 import { NotFound } from "./pages/NotFound/NotFound";
 import Layout from "./Layout";
+import Discover from "./pages/Discover/Discover";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route element={<Layout />}>
             <Route element={<LandingPage />} path="/" />
+            <Route element={<Discover />} path="/discoverevents" />
             <Route element={<NotFound />} path="*" />
           </Route>
         </Routes>{" "}
